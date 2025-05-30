d463e13cf23ea76737138d1776663532d166808e
besttxat <besttxat@gmail.com>
2025-05-30 10:41:11 +0700

    update

---
+++ src/app/(front)/product/page.tsx
@@ -0,0 +1,63 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import db from "@/db";
+import { product } from "@/db/schema";
+import { desc } from "drizzle-orm";
+import CartButton from "../components/CartButton";
+
+const Product = async () => {
+  const products = await db.query.product.findMany({
+    orderBy: [desc(product.id)]
+  });
+
+  return (
+    <div className="max-w-screen-xl mx-auto py-16 px-6 xl:px-0">
+      <div className="flex items-end justify-between">
+        <h2 className="text-3xl font-bold tracking-tight">Posts</h2>
+        <Select defaultValue="recommended">
+          <SelectTrigger className="w-[180px]">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="recommended">Recommended</SelectItem>
+            <SelectItem value="latest">Latest</SelectItem>
+            <SelectItem value="popular">Popular</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      <div className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
+        {products.map((i) => (
+          <Card key={i.id} className="shadow-none overflow-hidden rounded-md">
+            <CardHeader className="p-0">
+              <div className="aspect-video bg-muted w-full border-b" />
+            </CardHeader>
+            <CardContent className="py-6">
+              <div className="flex items-center gap-3">
+                <Badge className="bg-primary/5 text-primary hover:bg-primary/5 shadow-none">
+                  ราคา {i.price} บาท
+                </Badge>
+              </div>
+
+              <h2 className="mt-4 text-[1.35rem] font-semibold tracking-tight">
+                {i.title}
+              </h2>
+
+              <CartButton product={i} />
+
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/components/navbar-01/navbar-01.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import Link from "next/link";
+import { ShoppingBasket } from "lucide-react";
+import { Badge } from "../ui/badge";
+import CountCartItem from "@/app/(front)/components/CountCartItem";
+
+const Navbar01Page = () => {
+  return (
+    <div className="bg-muted">
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Logo />
+
+          {/* Desktop Menu */}
+          <NavMenu className="hidden md:block" />
+
+          <div className="flex items-center gap-3">
+
+          <Link href="/cart">
+            <Badge className="p-2 text-sm"><ShoppingBasket /> <CountCartItem /> item (s)</Badge>
+          </Link>
+
+            <Button asChild variant="outline" className="hidden sm:inline-flex">
+              <Link href="/login">เข้าสู่ระบบ</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/signup">สมัครสมาชิก</Link>
+            </Button>
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar01Page;
